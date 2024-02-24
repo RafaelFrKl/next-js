6957d61dc083077c25156b6b10e2a2ba5acd6b2b
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2024-02-23 16:53:12 -0800

    Profile Page

--- components/PromptCard.js
+++ components/PromptCard.js
@@ -6,6 +6,10 @@ import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState('');
 
   const handleCopy = () => {
@@ -57,6 +61,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='flex-center mt-5 gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='green_gradient cursor-pointer font-inter text-sm'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='orange_gradient cursor-pointer font-inter text-sm'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
